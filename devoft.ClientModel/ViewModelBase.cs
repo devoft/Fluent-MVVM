317ed4c6aae890f518f99f07541c9b6facd0a86b
mariodvm <mariodvm@gmail.com>
2018-09-23 18:04:11 +0200

    doc: basic documentation on ViewModelBase class

--- devoft.ClientModel/ViewModelBase.cs
+++ devoft.ClientModel/ViewModelBase.cs
@@ -23,6 +23,10 @@ namespace devoft.ClientModel
         protected internal Dictionary<string, IViewModelProperty> _values = new Dictionary<string, IViewModelProperty>();
         private Journal<IViewModelScopeTask<TInheritor>> _scopesJournal = new Journal<IViewModelScopeTask<TInheritor>>();
 
+        /// <summary>
+        /// Creates a ViewModel with a default dispatcher object
+        /// </summary>
+        /// <param name="dispatcher"></param>
         public ViewModelBase(IDispatcher dispatcher)
         {
             Dispatcher = dispatcher;
@@ -32,17 +36,43 @@ namespace devoft.ClientModel
         {
         }
 
+        /// <summary>
+        /// Used to initialize the ViewModel once the UI components are ready. 
+        /// It can be used to register commands, to set initial values to properties, etc
+        /// </summary>
+        /// <param name="dispatcher">Dispatcher object used to invoke this method in the UI thread</param>
         public virtual Task InitializeAsync(IDispatcher dispatcher = null)
             => Task.CompletedTask;
 
         internal static Dictionary<string, object> PropertyDescriptors { get; } = new Dictionary<string, object>();
 
+        /// <summary>
+        /// Return whether there are some validation errors
+        /// </summary>
         public bool HasErrors => _values.Values.Any(x => !x.ValidationResults.IsSucceded);
+        
+        /// <summary>
+        /// Dispatcher object used to update properties on the UI thread
+        /// </summary>
         public IDispatcher Dispatcher { get; set; }
 
+        /// <summary>
+        /// Event raised when any property value is changed
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Event raised when some errors happens in any property of the viewmodel.
+        /// </summary>
         public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
+        /// <summary>
+        /// It is intended to be used as the implementation of the <b>get</b> accessors of the properties on the inheritor ViewModel class. It returns the value set using <see cref="SetValue{T}(T, string)"/>
+        /// </summary>
+        /// <typeparam name="T">Type of the property</typeparam>
+        /// <param name="propertyName">Name of the property</param>
+        /// <param name="def">Initialization function used to return a default value when <see cref="SetValue{T}(T, string)"/> is not being called yet. When this method is used the <see cref="SetValue{T}(T, string)"/> is called to set the default value</param>
+        /// <returns>The Value set through <see cref="SetValue{T}(T, string)"/></returns>
         public T GetValue<T>([CallerMemberName] string propertyName = null, Func<T> def = null)
         {
             if (_values.TryGetValue(propertyName, out IViewModelProperty property))
@@ -56,6 +86,22 @@ namespace devoft.ClientModel
             return default(T);
         }
 
+        /// <summary>
+        /// It is intended to be used as the implementation of the <b>set</b> accessors of the properties on the inheritor ViewModel class. It returns the value set using <see cref="SetValue{T}(T, string)"/>
+        /// </summary>
+        /// <typeparam name="T">Type of the property</typeparam>
+        /// <param name="value">The value assigned to the property</param>
+        /// <param name="propertyName">Name of the property</param>
+        /// <remarks>
+        /// This method ensures the following in order:
+        /// - Apply validations on the property. <see cref="ViewModelPropertyDescriptor{TOwner, TResult}.ValidateAlways"/>, <see cref="HasErrors"/>, <see cref="GetValidationResults(string)"/> 
+        /// - Apply coerce on new values
+        /// - Validate coerced values if desired
+        /// - Assign value to the property
+        /// - Apply value setters on the property
+        /// - Record the property set if the property assignation is in an edition scope
+        /// - Notify property change when property is not in an edition scope
+        /// </remarks>
         protected virtual bool SetValue<T>(T value, [CallerMemberName] string propertyName = null)
         {
             if (!_values.TryGetValue(propertyName, out IViewModelProperty prop))
@@ -69,9 +115,21 @@ namespace devoft.ClientModel
             return ((ViewModelProperty<TInheritor, T>)prop).SetValue((TInheritor)this, propertyName, value);
         }
 
+        /// <summary>
+        /// Fluent entry point to configure the viewmodel's property specified through the param: <paramref name="exp"/> (e.g. RegisterProperty(myViewModel => myViewModel.Property)) 
+        /// </summary>
+        /// <typeparam name="TResult">Type of the property</typeparam>
+        /// <param name="exp">Property reference expression</param>
+        /// <returns>A <see cref="ViewModelPropertyDescriptor{TOwner, TResult}"/> describing the property specified through <paramref name="exp"/></returns>
         protected static ViewModelPropertyDescriptor<TInheritor, TResult> RegisterProperty<TResult>(Expression<Func<TInheritor, TResult>> exp)
             => RegisterProperty<TResult>((exp.Body as MemberExpression).Member.Name);
 
+        /// <summary>
+        /// Fluent entry point to configure the viewmodel's property specified through the param: <paramref name="propertyName"/> (e.g. RegisterProperty(nameof(Property))) 
+        /// </summary>
+        /// <typeparam name="TResult">Type of the property</typeparam>
+        /// <param name="propertyName">Property name</param>
+        /// <returns>A <see cref="ViewModelPropertyDescriptor{TOwner, TResult}"/> describing the property specified through <paramref name="exp"/></returns>
         protected internal static ViewModelPropertyDescriptor<TInheritor, TResult> RegisterProperty<TResult>(string propertyName)
         {
             ViewModelPropertyDescriptor<TInheritor, TResult> result;
@@ -82,24 +140,48 @@ namespace devoft.ClientModel
             return result;
         }
 
+        /// <summary>
+        /// Clear all existing validation results 
+        /// </summary>
         public void ClearValidationResults()
         {
             foreach (var val in _values.Values)
                 val.ValidationResults.Clear();
         }
 
+        /// <summary>
+        /// Return the validations results for the specified <paramref name="propertyName"/>
+        /// </summary>
+        /// <param name="propertyName">The name of the property validated</param>
+        /// <returns><see cref="ValidationResultCollection"/> containing the last validation results</returns>
         public ValidationResultCollection GetValidationResults(string propertyName)
             => _values[propertyName].ValidationResults;
 
+        /// <summary>
+        /// Notifies the change of the property specified on <paramref name="propertyName"/>. 
+        /// Also invalidates the registered commands in order to verify if they can be executed
+        /// </summary>
+        /// <param name="propertyName">Name of the property changed</param>
         public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             InvalidateCommands();
         }
 
+        /// <summary>
+        /// Called to raise the <see cref="ErrorsChanged"/> for the property <paramref name="propertyName"/>
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected internal virtual void OnErrorChanged([CallerMemberName] string propertyName = null)
             => ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
 
+        /// <summary>
+        /// Obtains the errors associated with the property specified by <paramref name="propertyName"/>. 
+        /// It's a filter over the method <see cref="GetValidationResults(string)"/>. 
+        /// If the <paramref name="propertyName"/> is null, then it searches for and returns all errors of every property registered 
+        /// </summary>
+        /// <param name="propertyName">The name of the property failing</param>
+        /// <returns>A collection of <see cref="ValidationResult"/> representing the errors ocurred on the property or class.</returns>
         public IEnumerable GetErrors(string propertyName)
             => string.IsNullOrWhiteSpace(propertyName)
                     ? _values.Values.SelectMany(v => v.ValidationResults.Where(vr => vr.Kind != ValidationKind.Success))
