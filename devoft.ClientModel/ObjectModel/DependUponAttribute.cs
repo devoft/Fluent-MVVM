634f03297eb37c0412439980ab71c5d72b8b6281
Johnna Menéndez <johnnamdez@gmail.com>
2018-09-23 19:12:17 +0200

    Bug fixed: Allow multiple DependUponAttribute

--- devoft.ClientModel/ObjectModel/DependUponAttribute.cs
+++ devoft.ClientModel/ObjectModel/DependUponAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace devoft.ClientModel.ObjectModel
 {
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
     public class DependUponAttribute : Attribute
     {
         public DependUponAttribute(string propertyName)
